7ecb7196fef5a6344e819c6aac7fa9eb2f72c319
arge01 <gevenci.arif@gmail.com>
2022-10-14 00:44:07 +0300

    404 found

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import Layout from 'component/Layout';
-
-import Link from 'next/link';
-
-function Custom404() {
+const 404 = () => {
   return (
-    <Layout className="d-flex justify-content-center align-items-center min-vh-100">
-      <p className="row w-100 align-items-center justify-content-center p-3">
-        Error
-        <span className="font-weight-bold text-light bg-dark p-1">404</span>
-        <Link href="/">
-          <a className="d-flex w-100 font-weight-bold text-dark align-items-center justify-content-center text-decoration-underline p-1">
-            go {`{home}`}
-          </a>
-        </Link>
-      </p>
-    </Layout>
+    <div>
+      404 yaaa
+    </div>
   );
-}
+};
 
-export default Custom404;
+export default 404;
