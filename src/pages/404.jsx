24ee14313340e9729338a1ec44d08295ae625345
arge01 <gevenci.arif@gmail.com>
2022-10-14 00:56:20 +0300

    custom404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const Custom404 = () => {
-  const a = "";
-  console.log(2);
+import Layout from 'component/Layout';
+
+import Link from 'next/link';
+
+function Custom404() {
   return (
-      <div>
-        404 yaaa
-      </div>
+    <Layout className="d-flex justify-content-center align-items-center min-vh-100">
+      <p className="row w-100 align-items-center justify-content-center p-3">
+        Error
+        <span className="font-weight-bold text-light bg-dark p-1">404</span>
+        <Link href="/">
+          <a className="d-flex w-100 font-weight-bold text-dark align-items-center justify-content-center text-decoration-underline p-1">
+            go {`{home}`}
+          </a>
+        </Link>
+      </p>
+    </Layout>
   );
-};
+}
 
 export default Custom404;
